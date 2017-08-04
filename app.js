96fa8e9be2041e704b16503c2673920141ca508a
runningapple <runningapple8@gmail.com>
2017-08-04 09:30:43 +0800

    添加登录功能

--- app.js
+++ app.js
@@ -5,8 +5,83 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+
+    this.login();
   },
 
+  login: function () {
+    var that = this
+    var token = that.globalData.token
+    if (token) {
+      wx.request({
+        url: 'http://127.0.0.1:3000',
+        data: {
+          token: token
+        },
+        success: function (res) {
+          if (res.data.code != 0) {
+            that.globalData.token = null;
+            that.login();
+          }
+        }
+      })
+      return;
+    }
+    wx.login({
+      success: function (res) {
+        wx.request({
+          url: '',
+          data: {
+            code: res.code
+          },
+          success: function (res) {
+            if (res.data.code == 10000) {
+              //register
+              that.registerUser();
+              return;
+            }
+            if (res.data.code != 0) {
+              //error
+              wx.hideLoading();
+              wx.showModal({
+                title: 'Tips',
+                content: 'Can not login',
+                showCancel: false
+              })
+              return;
+            }
+            that.globalData.token = res.datga.data.token;
+          }
+        })
+      }
+    })
+  },
+  registerUser: function () {
+    var that = this
+    wx.login({
+      success: function (res) {
+        var code = res.code;
+        wx.getUserInfo({
+          success: function (res) {
+            var iv = res.iv;
+            var encryptedData = res.encryptedData;
+            wx.request({
+              url: '',
+              data: {
+                code: code,
+                encryptedData: encryptedData,
+                iv: iv
+              },
+              success: (res) => {
+                wx.hideLoading();
+                that.login();
+              }
+            })
+          }
+        })
+      }
+    })
+  },
   getUserInfo: function(cb) {
     var that = this
     if (this.globalData.userInfo) {
